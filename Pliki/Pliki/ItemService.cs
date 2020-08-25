3b419d11663005b582663bac4a8623df55110d03
ZawadzkiKonrad <69811588+ZawadzkiKonrad@users.noreply.github.com>
2020-08-25 14:17:04 +0200

    Add files via upload

---
+++ Pliki/Pliki/ItemService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Warehouse;
+
+namespace Pliki
+{
+    public class ItemService
+    {
+        public List<Item> Items { get; set; }
+        public ItemService()
+        {
+            Items = new List<Item>();
+        }
+        public ConsoleKeyInfo AddNewItemView(MenuActionService actionService)
+        {
+            var addNewItemMenu = actionService.GetMenuActionsByMenuName("AddNewItemMenu");
+            Console.WriteLine("wybierz typ pliku:");
+            for (int i = 0; i < addNewItemMenu.Count; i++)
+            {
+                Console.WriteLine($"{addNewItemMenu[i].Id}.  {addNewItemMenu[i].Name}");
+            }
+
+            var operation = Console.ReadKey();
+            return operation;
+        }
+
+        public int AddNewItem(char itemType)
+        {
+            int itemTypeId;
+            Int32.TryParse(itemType.ToString(), out itemTypeId);
+            Item item = new Item();
+            item.TypeId = itemTypeId;
+            Console.WriteLine("podaj id nowego pliku");
+            var id = Console.ReadLine();
+            int itemId;
+            Int32.TryParse(id, out itemId);
+            Console.WriteLine("Podaj nazwe");
+            var name = Console.ReadLine();
+
+            item.Id = itemId;
+            item.Name = name;
+            Items.Add(item);
+            return itemId;
+        }
+
+        public void RemoveItem(int removeId)
+        {
+            Item productToRemove = new Item();
+            foreach (var item in Items)
+            {
+                if (item.Id==removeId)
+                {
+                    productToRemove = item;
+                    break;
+                }
+            }
+            Items.Remove(productToRemove);
+        }
+
+      
+
+        public int RemoveItemView()
+        {
+            Console.WriteLine("Podaj id pliku do usuniecia");
+            var ItemId = Console.ReadKey();
+            int id;
+            Int32.TryParse(ItemId.KeyChar.ToString(), out id);
+
+            return id;
+
+        }
+    }
+}
+
+
+
---
+++ Pliki/Pliki/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Warehouse;
+
+
+//29:21
+
+
+namespace Pliki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MenuActionService actionService = new MenuActionService();
+            actionService= Initialize(actionService);
+
+            Console.WriteLine("Witaj!!!");
+            Console.WriteLine("Wwybierz co chcesz zrobić z plikiem");
+            var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+            for (int i = 0; i < mainMenu.Count; i++)
+            {
+                Console.WriteLine($"{mainMenu[i].Id}.  {mainMenu[i].Name}");
+            }
+
+            var operation = Console.ReadKey();
+            ItemService itemService = new ItemService();
+            switch(operation.KeyChar)
+            {
+                case '1':
+                    var keyInfo = itemService.AddNewItemView(actionService);
+                    var id = itemService.AddNewItem(keyInfo.KeyChar);
+                    break;  
+                case '2':
+                    var removeId = itemService.RemoveItemView();
+                    itemService.RemoveItem(removeId);
+                    break;  
+                case '3':
+                    break; 
+                case '4':
+                    break;
+                default:
+                    Console.WriteLine("nieprawidłowa akcja!!");
+                    break;
+            }
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Dodaj plik", "Main");
+            actionService.AddNewAction(2, "Usuń plik", "Main");
+            actionService.AddNewAction(3, "Zobacz właściwości", "Main");
+            actionService.AddNewAction(4, "lista plików", "Main");
+
+            
+            actionService.AddNewAction(1, "Muzyka", "Main");
+            actionService.AddNewAction(2, "Video", "Main");
+            actionService.AddNewAction(3, "PDF", "Main");
+           
+            return actionService;
+        }
+    }
+}
